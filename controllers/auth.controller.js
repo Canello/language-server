2bb15d375549b740201f17ee6cdb71f36b214576
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-05-11 14:25:32 -0300

    Added input validation on signup.

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -35,6 +35,18 @@ exports.loginWithGoogle = async (req, res, next) => {
 exports.signup = async (req, res, next) => {
     const { fullName, email, password } = req.body;
 
+    // Validar inputs
+    if (fullName.length < 1)
+        throw new Error("O nome deve ter no mínimo 1 caracter.");
+    if (fullName.length > 100)
+        throw new Error("O nome deve ter no máximo 100 caracteres.");
+    if (email.length < 3) throw new Error("Email inválido.");
+    if (!email.includes("@")) throw new Error("Email inválido.");
+    if (password.length < 8)
+        throw new Error("A senha deve ter no mínimo 8 caracteres.");
+    if (password.length > 50)
+        throw new Error("A senha deve ter no máximo 50 caracteres.");
+
     // Checar se já existe um usuário cadastrado com esse email
     const existingUser = await User.findOne({ email });
     if (existingUser) throw new Error("Já existe um usuário com esse email.");
