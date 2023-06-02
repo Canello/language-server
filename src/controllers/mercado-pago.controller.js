91bea71b824d17340cc0514885d5572175586c01
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-06-02 14:00:26 -0300

    testing webhook

--- src/controllers/mercado-pago.controller.js
+++ src/controllers/mercado-pago.controller.js
@@ -1,5 +1,13 @@
+const axios = require("axios");
+
 exports.mercadopagoWebhook = async (req, res, next) => {
-    console.log(req.body);
+    const { id } = req.body;
+
+    const payment = await axios.get(
+        "https://api.mercadopago.com/v1/payments/" + id
+    );
+    console.log("will print payment");
+    console.log(payment);
 
     res.status(200).send("ok");
 };
