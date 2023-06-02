aa1d34a82026f0189a2eec0a9b21255291992946
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-06-02 13:28:28 -0300

    fixed path

--- src/middlewares/check-subscription.middleware.js
+++ src/middlewares/check-subscription.middleware.js
@@ -1,4 +1,4 @@
-const User = require("../models/User.model");
+const User = require("../models/user.model");
 
 exports.checkSubscription = async (req, res, next) => {
     const { userId } = req.headers;
