fade793af05d08c0070fd341e32f36f4fd79272f
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-06-30 15:17:20 -0300

    change free trials to 20

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
         fullName: { type: String, required: true },
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
-        freeTrials: { type: Number, default: 7 },
+        freeTrials: { type: Number, default: 20 },
         expiresAt: { type: Date, default: Date.now },
     },
     {
