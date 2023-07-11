9c2b872431589098882791c1ec2b90fb5fbb2341
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-07-11 12:17:11 -0300

    split app and initialization logic

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 require("dotenv").config();
 require("express-async-errors");
-
 const { transcription } = require("./controllers/transcription.controller");
 const { uploadAudio } = require("./middlewares/upload-audio.middleware");
 const { errorHandler } = require("./middlewares/error-handler.middleware");
@@ -28,12 +26,4 @@ app.use("/mercado-pago", mercadoPagoRouter);
 
 app.use(errorHandler);
 
-app.listen(process.env.PORT, async () => {
-    console.log("Listening on port", process.env.PORT);
-
-    await mongoose.connect(
-        `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.u6ck6wo.mongodb.net/?retryWrites=true&w=majority`
-    );
-
-    console.log("MongoDB is", mongoose.STATES[mongoose.connection.readyState]);
-});
+module.exports = { app };
