b33e75d677c41a0edd190d3e6efbbb0e6fcf1e3c
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-07-21 13:50:43 -0300

    trying to fix cors

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ require("express-async-errors");
 export const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.options("*", cors());
 
 app.use("/transcription", transcriptionRouter);
