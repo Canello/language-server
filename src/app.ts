bbbaf13401a1dbc1af6bcac041a9ffaf65a20f13
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-07-21 13:48:10 -0300

    enabling cors pre-flighting

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ export const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.options("*", cors());
 
 app.use("/transcription", transcriptionRouter);
 app.use("/chat", chatRouter);
